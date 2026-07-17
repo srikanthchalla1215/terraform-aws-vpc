0e6086875cbf07c6363a5c33b3b8a951c23a0375
shrikanthchalla <shrikanthchalla@gmail.com>
2026-07-17 20:28:48 +0530

    added

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_hostnames = true
+
+    tags = {
+        Name = "main"
+    }
+  
+}
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+    
+  tags = {
+    Name = "main"
+  }
+}
