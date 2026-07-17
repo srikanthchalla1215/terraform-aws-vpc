100a0c43cc0a82507d642468235f9d11d86a7468
shrikanthchalla <shrikanthchalla@gmail.com>
2026-07-17 21:15:08 +0530

    added

--- locals.tf
+++ locals.tf
@@ -1,4 +1,11 @@
 locals {
-  az_names = slice(data.aws_availability_zones.available.names, 0,2)
+    common_tags= {
+        project = var.project
+        Environment = var.environment
+        Terraform = true
+    }
+
+    
+    az_names = slice(data.aws_availability_zones.available.names, 0,2)
 
 }
