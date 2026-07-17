9752418b0d556e24d0892fc92b88e5979a36a1fd
shrikanthchalla <shrikanthchalla@gmail.com>
2026-07-17 21:21:40 +0530

    added

--- locals.tf
+++ locals.tf
@@ -5,7 +5,21 @@ locals {
         Terraform = true
     }
 
-    
+    vpc_final_tags = merge(local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    },
+    var.vpc_tags
+    )
+
+    igw_final_tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.igw_tags
+    )
+
     az_names = slice(data.aws_availability_zones.available.names, 0,2)
 
 }
--- main.tf
+++ main.tf
@@ -3,18 +3,14 @@ resource "aws_vpc" "main" {
     instance_tenancy = "default"
     enable_dns_hostnames = true
 
-    tags = {
-        Name = "main"
-    }
+    tags = local.vpc_final_tags
   
 }
 
 resource "aws_internet_gateway" "main" {
     vpc_id = aws_vpc.main.id
     
-  tags = {
-    Name = "main"
-  }
+  tags = local.igw_final_tags
 }
 
 
