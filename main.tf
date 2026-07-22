40be3542fa54df52f7967a0cac3d28207376a999
shrikanthchalla <shrikanthchalla@gmail.com>
2026-07-22 16:37:23 +0530

    adedd

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = var.vpc_cidr
     instance_tenancy = "default"
     enable_dns_hostnames = true
 
@@ -8,12 +8,11 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.main.id
-    
+  vpc_id = aws_vpc.main.id
   tags = local.igw_final_tags
 }
 
-
+# Public Subnets
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidr)
   vpc_id = aws_vpc.main.id
@@ -29,4 +28,136 @@ resource "aws_subnet" "public" {
   var.public_subnet_tags
   )
   
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidr)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(local.common_tags,
+  {
+    Name ="${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+  },
+  var.private_subnet_tags
+
+  )
+  
+}
+
+
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidr)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(local.common_tags,{
+    Name ="${var.project}-${var.environment}-database-${local.az_names[count.index]}"
+
+  },
+  var.database_subnet_tags
+  )
+
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-public"
+  },
+   var.public_route_table_tags
+  )
+
+}
+
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.common_tags, 
+  {
+  Name = "${var.project}-${var.environment}-private"
+  },
+  var.private_route_table_tags
+  )
+  
+}
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-database"
+  },
+  var.database_route_table_tags
+  )
+  
+}
+
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-nat"
+        },
+        var.eip_tags
+  )
+  
+}
+
+resource "aws_nat_gateway" "man" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws.aws_subnet.public[0].id
+
+  tags = merge(
+      local.common_tags,
+      {
+          Name = "${var.project}-${var.environment}"
+      },
+      var.nat_gateway_tags
+)
+  depends_on = [ aws_internet_gateway.main ]
+  
+}
+
+resource "aws_route" "private" {
+  route_table_id = aws_route_table.private
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.man.id
+  
+}
+
+resource "aws_route" "database" {
+  route_table_id = aws_route_table.database
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.man.id
+  
+}
+resource "aws_route_table_association" "public" {
+  count = length(var.database_subnet_cidr)
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+  
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidr)
+  subnet_id = aws_subnet.private[count.index].index
+  route_table_id = aws_route.private.id
+  
+}
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidr)
+  subnet_id = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
 }
--- variables.tf
+++ variables.tf
@@ -20,14 +20,66 @@ variable "environment" {
 
 }
 
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+  
+}
 
 variable "public_subnet_cidr" {
     type = list
     default = ["10.0.1.0/24", "10.0.2.0/24"]  
 }
 
+variable "private_subnet_cidr" {
+    type = list
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
+  
+}
+
+variable "database_subnet_cidr" {
+    type = list
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
+  
+}
 
 variable "public_subnet_tags" {
+    default = {}
+    type = map
+}
+
+variable "private_subnet_tags" {
+    default = {}
+    type = map
+}
+
+variable "database_subnet_tags" {
+    default = {}
+    type = map
+}
+
+
+variable "public_route_table_tags" {
+    default = {}
+    type = map
+}
+
+variable "private_route_table_tags" {
+    default = {}
+    type = map
+}
+
+variable "database_route_table_tags" {
+    default = {}
+    type = map
+}
+
+variable "eip_tags" {
+    default = {}
+    type = map
+}
+
+variable "nat_gateway_tags" {
     default = {}
     type = map
 }
