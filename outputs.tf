adc295d8acd3ca3950a62b8dbda33c5f938bfe8d
shrikanthchalla <shrikanthchalla@gmail.com>
2026-07-22 17:30:56 +0530

    update code

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "vpc" {
+    value = aws_vpc.main.id
+  
+}
+
+output "public_subnet_ids" {
+    value = aws.aws_subnet.public[*].id
+  
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
